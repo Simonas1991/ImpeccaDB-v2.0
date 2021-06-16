c1ef2c029d2ee215175c488b1ded468487fb9f20
Simonas1991 <simas.sniuksta@gmail.com>
2021-06-16 10:23:54 +0300

    feat: basic server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config({ path: 'server/.env' });
+const mongoose = require('mongoose');
+
+const server = express();
+const { SERVER_PORT, SERVER_URL } = process.env;
+
+// middleware
+server.use(cors());
+server.use(morgan('tiny'));
+server.use(express.json());
+
+// Main route
+server.get('/', (req, res) => {
+    res.send('Serveris veikia')
+});
+
+// mongoDB connection
+mongoose.connect(SERVER_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('connected', function () {
+    console.log('Connected to Database successfully!');
+    server.listen(SERVER_PORT, () => {
+        console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+    });
+});
+
+db.on('error', (error) => console.error('ERROR: FAILED TO CONNECT TO DB:\n ' + error));
